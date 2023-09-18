3e6260c28ce13d279ad6b2e699dda0f4f370ecfb
Kamil Khan <kamilkhan@rediffmail.com>
2023-09-18 07:20:48 +0530

    Modified the source - reducing unnecessary statements

--- lambda_function_extractor.py
+++ lambda_function_extractor.py
@@ -3,25 +3,25 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools import Tracer
-from aws_lambda_powertools import Metrics
-from aws_lambda_powertools.metrics import MetricUnit
-from aws_lambda_powertools.utilities import parameters
+
 import boto3
 import io
 import json
+
 app = APIGatewayRestResolver()
 tracer = Tracer()
 logger = Logger()
-metrics = Metrics(namespace="PowertoolsSample")
+
 @app.get("/get-text")
 @tracer.capture_method
 def get_text():
-    f_name = app.current_event["queryStringParameters"]['file-name']
-    metrics.add_metric(name="TextConversionInvocations", unit=MetricUnit.Count, value=1)
-    d = process_text_detection(f_name)
+    d = process_text_detection(app.current_event["queryStringParameters"]['file-name'])
     return d
+
 def get_triplet(block):
     return str(block.get('BlockType')), str(block.get('TextType')), str(block.get('Text'))
+
+
 def get_block_of_content(blocks, i, text_type):
     q = ""
     i = i + 1
@@ -31,10 +31,16 @@ def get_block_of_content(blocks, i, text_type):
         i = i + 1
         t = get_triplet(blocks[i])
     return q, i-1
+
+
 def get_printed_question(blocks, i):
     return get_block_of_content(blocks, i, "PRINTED")
+
+
 def get_handwritten_answer(blocks, i):
     return get_block_of_content(blocks, i, "HANDWRITING")
+
+
 def process_text_detection(f_name):
     blocks = boto3.client('textract', region_name='us-east-1').detect_document_text(
         Document={'S3Object': {'Bucket': "kamilxbucket", 'Name': f_name}})['Blocks']
@@ -51,9 +57,8 @@ def process_text_detection(f_name):
                     "question_no" : q_no,"question" : q,"answer" : a })
         i = i + 1
     return data_array
+
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
 @tracer.capture_lambda_handler
-@metrics.log_metrics(capture_cold_start_metric=True)
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
-    logger.info(event)
     return app.resolve(event, context)
